3dd47cb96d51fafdaf84eb423f3652bd876fdffa
Mahmoud Ahmed <mahmoud.rod11@gmail.com>
2024-06-29 18:18:01 +0300

    updated

--- CSV_MVC/CSV_MVC/obj/Debug/net8.0/CSV_MVC.AssemblyInfo.cs
+++ CSV_MVC/CSV_MVC/obj/Debug/net8.0/CSV_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSV_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08d2cb2533592747c262159f847d37c16c7e75e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSV_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSV_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
